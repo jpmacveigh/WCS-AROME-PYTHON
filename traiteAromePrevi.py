b9b8b143625f339ac908595dab4b69daec860824
Jean Pierre MAC VEIGH <jpmacveigh@hotmail.fr>
2021-01-26 06:05:46 +0000

    Upload de Arome.sqlite vers S3

--- traiteAromePrevi.py
+++ traiteAromePrevi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # coding: utf8
+import boto3
 import sqlite3
 import json
 import os
@@ -7,7 +8,8 @@ from dateLimiteRetention import dateLimiteRetention
 # Lecture du fichier previArome.txt lignes par lignes et écriture dans la base Arome.sqlite
 def traiteAromePrevi():
     repcourant=os.getcwd()+"/"
-    con = sqlite3.connect(repcourant+"Arome.sqlite")
+    sqlite_name=repcourant+"Arome.sqlite"
+    con = sqlite3.connect(sqlite_name)
     cur = con.cursor()
     #cur.execute("DELETE from prevision")
     con.commit()
@@ -22,10 +24,12 @@ def traiteAromePrevi():
         cur.execute("INSERT INTO prevision (now,nom,abrev,niv,hauteur,unit,run,date,val) VALUES(?,?,?,?,?,?,?,?,?)",[previ["now"],previ["nom"],previ["abrev"],previ["niv"],previ["z"],previ["unit"], previ["run"],previ["date"],previ["val"]])
     con.commit()
     dateLimite=dateLimiteRetention(40)  # on efface les données plus vieilles de 40 heures
-    cmd='DELETE FROM prevision WHERE now <= "'+dateLimite+'"'
-    print (cmd)
-    cur.execute(cmd)
+    #cmd='DELETE FROM prevision WHERE now <= "'+dateLimite+'"'
+    #print (cmd)
+    #cur.execute(cmd)
     con.commit()
     con.close()
     fic.close()
-#traiteAromePrevi()    
+    s3=boto3.client("s3")
+    s3.upload_file(sqlite_name, "elasticbeanstalk-eu-west-1-062282685834", "Arome.sqlite") # upload de la base sqlite vers S3
+traiteAromePrevi()    
